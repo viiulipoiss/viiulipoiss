86b6eca6bef31bd2b1c446811fb96bae62dc3fb7
viiulipoiss <167100206+viiulipoiss@users.noreply.github.com>
2024-10-10 16:39:41 +0300

    Add files via upload
    Hack Message

---
+++ GUI.exe/gui1.0.bat
@@ -0,0 +1,3 @@
+@echo off
+echo welcome
+pause
---
+++ GUI.exe/gui2.0.bat
@@ -0,0 +1,20 @@
+@echo off
+:menu
+echo.
+echo 1) Notification
+echo 2) Message Box
+set /p input=">>"
+if %input% EQU 1 goto noti
+if %input% EQU 2 goto msgbox
+:msgbox
+cls
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Windows', 'Upgrading from Windows 11 to Windows 12...', 'OK', [System.Windows.Forms.ToolTipIcon]::None)}"
+cls
+goto menu
+
+
+:noti
+cls
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Error; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Windows Defender', '8 Trojans have been detected!.', [System.Windows.Forms.ToolTipIcon]::None)}"
+cls
+goto menu
